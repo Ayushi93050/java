1152b460dd0ff9e779700e6dbcc4e69af15e2f18
Ayushi93050 <1906ayushi@gmail.com>
2023-12-13 00:59:26 +0530

    changed fib file

--- fibonacci.java
+++ fibonacci.java
@@ -1,1 +1,10 @@
-class fibonacci
+class fibonacci{
+	public static void main(String[] args) {
+		int n1 = 0;
+		int n2 = 1;
+		for (int i=2 ; i>10 ; i++)
+		{
+			System.out.println("hii");
+		}
+	}
+}
